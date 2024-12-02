b136fe8300e2f69e15884bd781860dbc734d8ad1
dogaomf <douglasmfcomercial@gmail.com>
2024-12-02 20:45:38 -0300

    commit incial do projeto

---
+++ udidrive/src/main/java/br/com/udidrive/UdiDriveApplication.java
@@ -0,0 +1,13 @@
+package br.com.udidrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdiDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdiDriveApplication.class, args);
+	}
+
+}
---
+++ udidrive/src/test/java/br/com/udidrive/UdiDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udidrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdiDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
